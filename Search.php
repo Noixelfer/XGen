4cacfa02995778aa8c646b42ea5e48f3ba12f6f3
Oana Bocut - SINF 2016 <onutzzaa12@yahoo.com>
2018-05-11 18:13:27 +0300

    Created Search UI

--- Search.php
+++ Search.php
@@ -1,9 +1,43 @@
 <!DOCTYPE html>
-<html>
-<body>
+<html lang="en">
+<head>
+    <html class="no-js" lang="en" dir="ltr"/>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
+    <link type="text/css" rel="stylesheet" href="Search.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Amita' rel='stylesheet'/>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <title>XGen</title>
+    </head>
+<body background="http://www.sjhemleymarketing.com/wp-content/uploads/2015/04/iStock_000041965580_Full.jpg" class="img1">
+<div class="container">
+    <form class="f1">
+    <div class="row">
+        <div class="col-sm-12">
+            <!--5 *<br/>-->
+            <h1 style="font-family: Amita" class="text-center"><b class="titlu_culoare">Research</b></h1>
+        </div>
+    </div>
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <form class="navbar-form" role="search">
+                    <div class="input-group add-on">
+                        <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+    </form>
+</div>
 <?php
-echo "My first PHP script :D 0000!";
+//echo "My first PHP script :D 0000!";
 ?>
 
 </body>
